d57b8351f49ca630502336e5ec79e8517d52cf00
Emery Bosc <emerybosc@MacBook-Pro-de-Emery.local>
2023-06-19 13:28:26 -0400

    Supression d'Extract.py pour un transfert dans un autre dossier et création du premier DF

--- src/Extract.py
+++ src/Extract.py
@@ -1,14 +1,18 @@
 import pandas as pd
-import requests
 import json
 from pyspark.sql import SparkSession
 import requests
+from pyspark.sql.functions import explode, col
+from pyspark.sql.functions import arrays_zip
+
+
+
 
 spark = SparkSession.builder \
     .appName("LH") \
     .getOrCreate()
 
-url = "https://api.lufthansa.com/v1/mds-references/airlines?limit=20&offset=0"
+url = "https://api.lufthansa.com/v1/mds-references/airlines?limit=100&offset=0"
 headers = {
     "Authorization": "Bearer 6d4rmazx5bnu9jy6fejtxpzf",
     "Accept": "application/json"
@@ -18,5 +22,14 @@ response = requests.get(url, headers=headers)
 airportJson = response.json()
 
 dfRawAirport = spark.read.json(spark.sparkContext.parallelize([airportJson]))
-dfRawAirport.show()
+
 dfRawAirport.printSchema()
+
+
+explodedDf = dfRawAirport.withColumn("combined", explode(arrays_zip(col("AirlineResource.Airlines.Airline.AirlineID"),
+                                                                   col("AirlineResource.Airlines.Airline.AirlineID_ICAO"),
+                                                                   col("AirlineResource.Airlines.Airline.Names.Name.$"),
+                                                                   col("AirlineResource.Airlines.Airline.Names.Name.@LanguageCode"))))
+transformedDf = explodedDf.select("combined.*").toDF("AirlineID", "AirlineID_ICAO", "Name", "LanguageCode").drop("AirlineResource","AirlineResource.meta")
+transformedDf.show()
+transformedDf.printSchema()

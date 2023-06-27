f514eadc20c01d100ccd76c27e1850b868a19fa5
Emery Bosc <emerybosc@MacBook-Pro-de-Emery.local>
2023-06-27 09:39:36 -0400

    Ajout commentaire

--- src/Extract.py
+++ src/Extract.py
@@ -7,22 +7,25 @@ from pyspark.sql.functions import arrays_zip, struct
 import math
 import time 
 
-
+# Initialisation de la Session Spark 
 spark = SparkSession.builder \
     .appName("LH") \
     .getOrCreate()
 
-
+# Information sur l'en tête pour les call API 
 headers = {
     "Authorization": "Bearer g36yfg4cmtdzx9nxhzyu2k95",
     "Accept": "application/json"
 }
 
+# Url de l'API pour recupere les types d'aéronefs, les aéroports et les compagnies aériennes
 apiUrls = [
     ("https://api.lufthansa.com/v1/mds-references/airlines?limit=100&offset=0", "airlines.json", "AirlineResource", "Airlines"),
     ("https://api.lufthansa.com/v1/mds-references/airports/?limit=100&offset=0&LHoperated=1", "airport.json", "AirportResource", "Airports"),
     ("https://api.lufthansa.com/v1/mds-references/aircraft/?limit=100&offset=0", "aircraft.json", "AircraftResource", "AircraftSummaries")
 ]
+
+# Effectue les calls API, itère sur chaque page de résultats et écrit les données dans un fichier JSON 
 def fetchIterateAndWrite(url, filename, resourceKey, dataKey): 
     delaySec = 1
     pageNumber = 1
@@ -53,12 +56,13 @@ def fetchIterateAndWrite(url, filename, resourceKey, dataKey):
     with open(filename, "w") as writer:
        writer.write(json.dumps(fusedData))
 
-
+# Boucle d'appel a la fonction fetchIterateAndWrite pour récuperer les données des urls de apiUrls
 #for url, filename, resourceKey, dataKey in apiUrls:
 #    fetchIterateAndWrite(url, filename, resourceKey, dataKey)
 
 output_path = "airline"
 
+# Fonction qui prends en argument un DataFrame brut et un schéma de colonne puis effectue une transformation des colonnes à partir du schéma 
 def transform_df(raw_df, column_mapping):
     exploded_df = raw_df.withColumn("combined", explode(arrays_zip(*column_mapping.keys())))
     transformed_df = exploded_df.select("combined.*").toDF(*column_mapping.values())
@@ -70,7 +74,7 @@ column_mapping_airline = {
     "Airline.AirlineID": "AirlineID",
     "Airline.AirlineID_ICAO": "AirlineID_ICAO",
     "Airline.Names.Name.$": "Name",
-    "Airline.Names.Name.@LanguageCode": "LanguageCode"
+    "Airline.Names.Name.@LanguageCode": "s"
 }
 transformedAirlineDf = transform_df(dfRawAirline, column_mapping_airline)
 transformedAirlineDf.drop("AirlineResource", "AirlineResource.meta").write.csv(output_path, mode="overwrite", header=True)
@@ -104,17 +108,20 @@ transformedAircraftDf.write.csv("aircraft", mode="overwrite", header=True)
 
 
 
-
+# Url pour récuperer le calendrier de vol du 21 JUINS 2023 au 22 Juillet 
 apiUrl2 = "https://api.lufthansa.com/v1/flight-schedules/flightschedules/passenger?airlines=LH&startDate=21JUN23&endDate=22JUL23&daysOfOperation=1234567&timeMode=UTC"
 
+# Récupération des données dans un fichier JSON de la requêtes HTTP 
 response = requests.get(apiUrl2, headers=headers)
 dataJson = response.json()
 with open("flight_schedule.json", "w") as writer:
        writer.write(json.dumps(dataJson))
 
+
 dfRawSchedule = spark.read.json("flight_schedule.json")
 dfRawSchedule.printSchema()
 
+# Décomposition des données du tableau imbriqué avec explode 
 explodeLHScheduledDf = dfRawSchedule.select(
     "airline", "flightNumber", "legs", "periodOfOperationUTC.startDate", "periodOfOperationUTC.endDate"
 ).withColumn(
@@ -143,12 +150,13 @@ explodeLHScheduledDf = dfRawSchedule.select(
     )
 )
 
+# Selection des colonnes d'interêt et rennomage des colonnes 
 transformedLHScheduleDf = explodeLHScheduledDf.select("combined.*").toDF(
     "ArrivalTimeUTC", "ArrivalTimeVariation", "DepartureTimeUTC", "aircraftOwner", "aircraftType",
     "destination", "origin", "registration", "sequenceNumber", "periodOfOperationUTC"
 )
 
-
+#Transformation des données UTC en un format horaire standard HH:MM par concaténation de 2 opérations
 scheduleDF = transformedLHScheduleDf.select(
     "*",
     expr("concat(lpad(DepartureTimeUTC div 60, 2, '0'), ':', lpad(DepartureTimeUTC % 60, 2, '0'))").alias("departureTime"),

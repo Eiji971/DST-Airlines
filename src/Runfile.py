9eac19c0608f8ddd84febd312631aaf7f48d020d
Emery Bosc <emerybosc@MacBook-Pro-de-Emery.local>
2023-07-18 21:02:43 -0400

    Changement de chemin de fichier

--- src/Runfile.py
+++ src/Runfile.py
@@ -9,43 +9,43 @@ import ExtractFunctionsDST
 
 # List of URL fro API call, file name and data Key 
 apiUrls = [
-    ("https://api.lufthansa.com/v1/mds-references/airlines?limit=100&offset=0", "airlines.json", "AirlineResource", "Airlines"),
-    ("https://api.lufthansa.com/v1/mds-references/airports/?limit=100&offset=0&LHoperated=1", "airport.json", "AirportResource", "Airports"),
-    ("https://api.lufthansa.com/v1/mds-references/aircraft/?limit=100&offset=0", "aircraft.json", "AircraftResource", "AircraftSummaries")
+    ("https://api.lufthansa.com/v1/mds-references/airlines?limit=100&offset=0", "./data/extractedjson/airlines.json", "AirlineResource", "Airlines"),
+    ("https://api.lufthansa.com/v1/mds-references/airports/?limit=100&offset=0&LHoperated=1", "./data/extractedjson/airport.json", "AirportResource", "Airports"),
+    ("https://api.lufthansa.com/v1/mds-references/aircraft/?limit=100&offset=0", "./data/extractedjson/aircraft.json", "AircraftResource", "AircraftSummaries")
 ]
 
 # Information on the headers for API calls
 headers = {
-    "Authorization": "Bearer jbu7hy83jbdfjsk692hbys9f",
+    "Authorization": "Bearer urbsgx2u2k9mz4tw7xmtd633",
     "Accept": "application/json"
 }
 
-
+"""""
 for url, filename, resource_key, data_key in apiUrls:
    ExtractFunctionsDST.fetch_iterate_and_write(url, filename, resource_key, data_key)
-
+"""""
 
 # Processing of Airline Data 
-fileName = "airlines.json"
+fileName = "./data/extractedjson/airlines.json"
 airlines_df = ExtractFunctionsDST.process_airline_data(fileName)
 print(airlines_df)
 
 
 # Processing of Airport Data 
-fileName = "airport.json"
+fileName = "./data/extractedjson/airport.json"
 airport_df = ExtractFunctionsDST.process_airport_data(fileName)
 print(airport_df)
 
 
 # Processing of Aircraft Data 
-fileName = "aircraft.json"
+fileName = "./data/extractedjson/aircraft.json"
 aircraft_df = ExtractFunctionsDST.process_aircraft_data(fileName)
 print(aircraft_df)
 
 
 apiUrl2 = "https://api.lufthansa.com/v1/flight-schedules/flightschedules/passenger?airlines=LH&startDate=21JUN23&endDate=22JUL23&daysOfOperation=1234567&timeMode=UTC"
 
-fileName = "flight_schedule.json"
+fileName = "./data/extractedjson/flight_schedule.json"
 
 # Make the HTTP request and save the data to a JSON file
 response = requests.get(apiUrl2, headers=headers)
@@ -70,7 +70,7 @@ destination = unique_combinations['destination'].tolist()
 
 today = datetime.now().date()
 formattedDate = today.strftime("%Y-%m-%d")
-fileName = f'flight_status1{formattedDate}.json'
+fileName = f'./data/extractedjson/flight_status1{formattedDate}.json'
 
 # Retrieval of Flight Status from the day 
 response_flight_info = ExtractFunctionsDST.fetch_flight_information(origin, destination, formattedDate, headers)

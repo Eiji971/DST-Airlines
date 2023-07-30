7c857ac1df2ec563f66a0952378f20169e6b41eb
Emery Bosc <emerybosc@MacBook-Pro-de-Emery.local>
2023-07-30 12:34:50 -0400

    link change and add auto token retrieval

--- src/base_data_extract.py
+++ src/base_data_extract.py
@@ -6,37 +6,43 @@ import requests
 from datetime import datetime
 import os 
 import ExtractFunctionsDST
+from Authentication_key_retrieval import get_valid_token
 
 # List of URL fro API call, file name and data Key 
 apiUrls = [
-    ("https://api.lufthansa.com/v1/mds-references/airlines?limit=100&offset=0", "./data/extractedjson/airlines.json", "AirlineResource", "Airlines"),
-    ("https://api.lufthansa.com/v1/mds-references/airports/?limit=100&offset=0&LHoperated=1", "./data/extractedjson/airport.json", "AirportResource", "Airports"),
-    ("https://api.lufthansa.com/v1/mds-references/aircraft/?limit=100&offset=0", "./data/extractedjson/aircraft.json", "AircraftResource", "AircraftSummaries")
+    ("https://api.lufthansa.com/v1/mds-references/airlines?limit=100&offset=0", "/data/extractedjson/airlines.json", "AirlineResource", "Airlines"),
+    ("https://api.lufthansa.com/v1/mds-references/airports/?limit=100&offset=0&LHoperated=1", "/data/extractedjson/airport.json", "AirportResource", "Airports"),
+    ("https://api.lufthansa.com/v1/mds-references/aircraft/?limit=100&offset=0", "/data/extractedjson/aircraft.json", "AircraftResource", "AircraftSummaries")
 ]
 
+
+bearer_token = get_valid_token()
 # Information on the headers for API calls
 headers = {
-    "Authorization": "Bearer urbsgx2u2k9mz4tw7xmtd633",
-    "Accept": "application/json"
-}
+        "Authorization": f"Bearer {bearer_token}",
+        "Accept": "application/json"
+    }
+
+
 
 for url, filename, resource_key, data_key in apiUrls:
    ExtractFunctionsDST.fetch_iterate_and_write(url, filename, resource_key, data_key)
 
 
+
 # Processing of Airline Data 
-fileName = "./data/extractedjson/airlines.json"
+fileName = "/data/extractedjson/airlines.json"
 airlines_df = ExtractFunctionsDST.process_airline_data(fileName)
 print(airlines_df)
 
 
 # Processing of Airport Data 
-fileName = "./data/extractedjson/airport.json"
+fileName = "/data/extractedjson/airport.json"
 airport_df = ExtractFunctionsDST.process_airport_data(fileName)
 print(airport_df)
 
 
 # Processing of Aircraft Data 
-fileName = "./data/extractedjson/aircraft.json"
+fileName = "/data/extractedjson/aircraft.json"
 aircraft_df = ExtractFunctionsDST.process_aircraft_data(fileName)
 print(aircraft_df)
